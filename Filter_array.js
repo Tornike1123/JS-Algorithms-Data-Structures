8e78d325a87caa2686464d89113ea41a6707ef21
Tornike Khatiashvili <toko36106@gmail.com>
2023-07-17 14:27:14 +0400

    filter array

---
+++ Filter_array.js
@@ -0,0 +1,15 @@
+
+//
+
+let arr = [1,0,2,0,4,5,6,0,2]
+let arrr = []
+let arrr2 = []
+
+
+
+arrr = arr.filter(el => el === 0)
+arrr2 = arr.filter(el => el !== 0)
+
+let resp = [...arrr2, ...arrr]
+
+console.log(resp);
--- Inter_Section.js
+++ Inter_Section.js
@@ -1,4 +1,4 @@
-//თანაკვეთა (ამოიღოს ერთნაირი ელემენტები)
+// თანაკვეთა (ამოიღოს ერთნაირი ელემენტები)
 
 let arr = [0,1,2,3]
 let arr2 = [2,3,5,6]
@@ -9,3 +9,6 @@ if(arr2.includes(index))
  arrPush.push(index)
 }
 console.log(arrPush);
+
+
+
