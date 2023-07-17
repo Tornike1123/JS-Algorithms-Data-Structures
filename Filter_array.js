3e9a616f613a35242ad0d19d8cbad006334dfc93
Tornike Khatiashvili <toko36106@gmail.com>
2023-07-17 14:52:21 +0400

    version 2 filter array

--- Filter_array.js
+++ Filter_array.js
@@ -1,6 +1,8 @@
 
 //0 ის ამოღება და ბოლოში გადატანა
 
+
+//V1
 let arr = [1,0,2,0,4,5,6,0,2]
 let arrr = []
 let arrr2 = []
@@ -13,3 +15,11 @@ arrr2 = arr.filter(el => el !== 0)
 let resp = [...arrr2, ...arrr]
 
 console.log(resp);
+
+
+//V2 Best version
+
+let arr2 = [1, 0, 2, 0, 4, 5, 6, 0, 2];
+let combinedArr = arr2.filter(el => el !== 0).concat(arr2.filter(el => el === 0));
+
+console.log(combinedArr);
