3c1e3b9da68b102212b5975e06135e4a162dfd88
Tornike Khatiashvili <toko36106@gmail.com>
2023-07-25 17:27:06 +0400

    ...

--- Massive_example.js
+++ Massive_example.js
@@ -1,10 +1,12 @@
 
 // The first letter should start with a capital letter and the rest should be lowercase
 let arr = ["tOko","GioRgi"]
+const arrPush = []
 
 let mm = arr.map((o)=> {
     const m2 = o.slice(0,1).toUpperCase() + (o.slice(1).toLocaleLowerCase())
+    arrPush.push(m2)
 
-    console.log(m2);
- 
+    
 })
+console.log(arrPush);
