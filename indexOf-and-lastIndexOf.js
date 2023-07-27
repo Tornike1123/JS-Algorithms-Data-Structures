e49972e118492ddcd186b3f7e47805077b2a83c1
Tornike Khatiashvili <toko36106@gmail.com>
2023-07-27 11:40:29 +0400

    indexOf and lastIndexOf

---
+++ indexOf-and-lastIndexOf.js
@@ -0,0 +1,5 @@
+const names = ['florin', 'ivan', 'jay', 'ivan', 'smith']
+const idx = names.indexOf('ivan') //პირველი რომელიც შეხვდება იმას დაინახავს(იმის ინდექს ამოიღებს)
+const idx2 = names.lastIndexOf('ivan') //ბოლოს რომელიც შეხვდება იმას დაინახავს(იმის ინდექს ამოიღებს)
+console.log(idx);
+console.log(idx2);
