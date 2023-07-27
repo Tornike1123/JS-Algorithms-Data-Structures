b862f5b19a495299d167fe4c16ed0c1acd2f4e63
Tornike Khatiashvili <toko36106@gmail.com>
2023-07-27 11:34:24 +0400

    unshift and shift

---
+++ shift-and-unshift.js
@@ -0,0 +1,8 @@
+const arr = [1,2,3,4]
+arr.unshift(0)
+console.log(arr); // ამატებს პირველ ელემენტად (თავში)
+
+
+const arr2 = [1,2,3,4]
+arr2.shift()
+console.log(arr2); // იღებს პირველ ელემენტს (თავში)
