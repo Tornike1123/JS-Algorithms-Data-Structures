ba52f1232b45b49298cc035a3a9e3da97c89ffe0
Tornike Khatiashvili <toko36106@gmail.com>
2023-07-27 11:18:56 +0400

    reverse

---
+++ reverse.js
@@ -0,0 +1,12 @@
+//V1
+
+const str = 'Codingg is Fun!';
+
+const res = str.split('').reverse().join("")
+
+console.log(res);
+
+//V2
+const arr = [1,2,3,"a","b"]
+const result = arr.reverse()
+console.log(result);
